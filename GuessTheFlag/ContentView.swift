ecc1c74580c8e8d6337fe2c91566f5d54cac891e
David Bailey <davidbailey00@outlook.com>
2021-05-17 09:26:47 +0100

    Create prompt and display flag images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    let countries = [
+        "Estonia", "France", "Germany", "Ireland", "Italy", "Monaco",
+        "Nigeria", "Poland", "Russia", "Spain", "UK", "US"
+    ]
+    var correctAnswer = Int.random(in: 0 ... 2)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 32) {
+                VStack {
+                    Text("Tap the flag of: \(countries[correctAnswer])")
+                }
+
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        // flag was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+
+                Spacer()
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
